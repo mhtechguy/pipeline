e1b1c8a06a1c6b83cf2e780b44a74aed690c0c6a
Bert Frees <bertfrees@gmail.com>
2018-07-30 11:20:48 +0200

    git subrepo pull framework
    subrepo:
      subdir:   "framework"
      merged:   "5108a05095"
    upstream:
      origin:   "git@github.com:daisy/pipeline-framework.git"
      branch:   "master"
      commit:   "9dc486b912"
    git-subrepo:
      version:  "0.3.1"
      origin:   "???"
      commit:   "???"

---
+++ framework/modules-registry/src/main/java/org/daisy/pipeline/modules/AbstractModuleBuilder.java
@@ -0,0 +1,156 @@
+package org.daisy.pipeline.modules;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import org.daisy.pipeline.modules.Component;
+import org.daisy.pipeline.modules.Entity;
+import org.daisy.pipeline.modules.Module;
+import org.daisy.pipeline.modules.ResourceLoader;
+import org.daisy.pipeline.xmlcatalog.XmlCatalog;
+import org.daisy.pipeline.xmlcatalog.XmlCatalogParser;
+
+import org.osgi.framework.FrameworkUtil;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class AbstractModuleBuilder<T extends AbstractModuleBuilder> {
+	
+	protected abstract T self();
+	
+	private String name;
+	private String version;
+	private String title;
+	private ResourceLoader loader;
+	private final List<Component> components = new ArrayList<Component>();
+	private final List<Entity> entities = new ArrayList<Entity>();
+	private final Logger mLogger = LoggerFactory.getLogger(getClass());
+	
+	public Module build() {
+		return new Module(name, version, title, components, entities);
+	}
+	
+	public T withName(String name) {
+		this.name = name;
+		return self();
+	}
+	
+	public T withLoader(ResourceLoader loader) {
+		this.loader = loader;
+		return self();
+	}
+	
+	public T withVersion(String version) {
+		this.version = version;
+		return self();
+	}
+	
+	public T withTitle(String title) {
+		this.title = title;
+		return self();
+	}
+	
+	public T withComponents(Collection<? extends Component> components) {
+		this.components.addAll(components);
+		return self();
+	}
+	
+	public T withComponent(URI uri, String path) {
+		mLogger.trace("withComponent:" + uri.toString() + ", path: " + path);
+		components.add(new Component(uri, path, loader));
+		return self();
+	}
+	
+	public T withEntities(Collection<? extends Entity> entities) {
+		this.entities.addAll(entities);
+		return self();
+	}
+	
+	public T withEntity(String publicId, String path) {
+		mLogger.trace("withEntity:" + publicId.toString() + ", path: " + path);
+		entities.add(new Entity(publicId, path, loader));
+		return self();
+	}
+	
+	public T withCatalog(XmlCatalog catalog) {
+		for (Map.Entry<URI, URI> entry : catalog.getSystemIdMappings().entrySet()) {
+			withComponent(entry.getKey(), entry.getValue().toString());
+		}
+		for (Map.Entry<URI, URI> entry : catalog.getUriMappings().entrySet()) {
+			withComponent(entry.getKey(), entry.getValue().toString());
+		}
+		for (Map.Entry<String, URI> entry : catalog.getPublicMappings().entrySet()) {
+			withEntity(entry.getKey(), entry.getValue().toString());
+		}
+		for (Map.Entry<URI, URI> rule : catalog.getRewriteUris().entrySet()) {
+			Iterable<URL> entries = this.loader.loadResources(rule.getValue().toString());
+			for (URL url : entries) {
+				try {
+					//get tail of the path i.e. ../static/css/ -> /css/
+					String path = url.toURI().getPath().toString().replace(rule.getValue().toString().replace("..",""),"");
+					withComponent(rule.getKey().resolve(URI.create(path)), url.toString());
+				} catch (URISyntaxException e) {
+					mLogger.warn("Exception while generating paths");
+				}
+			}
+		}
+		return self();
+	}
+	
+	/*
+	 * Use loader to find catalog.xml file
+	 */
+	public T withCatalogParser(XmlCatalogParser parser) {
+		if (loader == null)
+			throw new UnsupportedOperationException("Resource loader not set");
+		URL catalogURL = loader.loadResource("../META-INF/catalog.xml");
+		if (catalogURL == null)
+			throw new RuntimeException("/META-INF/catalog.xml file not found");
+		try {
+			return withCatalog(parser.parse(asURI(catalogURL)));
+		} catch (URISyntaxException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	/*
+	 * Return either a JarModuleBuilder or a OSGIModuleBuilder
+	 */
+	public static AbstractModuleBuilder fromContainedClass(Class<?> clazz) {
+		try {
+			URI jarFileURI = clazz.getProtectionDomain().getCodeSource().getLocation().toURI();
+			try {
+				File jarFile = new File(jarFileURI);
+				return new JarModuleBuilder().withJarFile(jarFile);
+			} catch (IllegalArgumentException e) {
+				// Could be because we are running in OSGi context
+				return OSGiHelper.getOSGiModuleBuilder(clazz);
+			}
+		} catch (URISyntaxException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	private static URI asURI(URL url) throws URISyntaxException {
+		if (url.getProtocol().equals("jar"))
+			return new URI("jar:" + new URI(null, url.getAuthority(), url.getPath(), url.getQuery(), url.getRef()).toASCIIString());
+		String authority = (url.getPort() != -1) ?
+			url.getHost() + ":" + url.getPort() :
+			url.getHost();
+		return new URI(url.getProtocol(), authority, url.getPath(), url.getQuery(), url.getRef());
+	}
+	
+	// static nested class in order to delay class loading
+	private static abstract class OSGiHelper {
+		static AbstractModuleBuilder getOSGiModuleBuilder(Class<?> clazz) {
+			return new OSGIModuleBuilder().withBundle(FrameworkUtil.getBundle(clazz));
+		}
+	}
+}
